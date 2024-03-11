c1cb14d89308d44cd9d43ca6cc61a3d3d7cfdecb
joethibault <158072894+joethibault@users.noreply.github.com>
2024-03-11 09:54:23 -0400

    Update replay.js
    Added "Tab" to ignore list.

--- dist/replay.js
+++ dist/replay.js
@@ -150,7 +150,7 @@ class Replay {
                 let lastSpace = textOutput.lastIndexOf(' ');
                 return textOutput.slice(0, lastSpace);
             default:
-                return !["Shift", "Ctrl", "Delete", "Alt", "ArrowDown", "ArrowUp", "Control", "ArrowRight", "ArrowLeft"].includes(key) ? textOutput + key : textOutput;
+                return !["Shift", "Ctrl", "Delete", "Alt", "Tab", "ArrowDown", "ArrowUp", "Control", "ArrowRight", "ArrowLeft"].includes(key) ? textOutput + key : textOutput;
         }
     }
 }
